a58a08b1f18e60dd09220d3edababa017612b398
Ivanna Fryshchyn <ivanna.fryshchyn@gmail.com>
2021-12-15 14:24:32 +0200

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("x+y-a/2*(x+w)\na*(x+x)/y-a*3\na+b/x-y\nX-e/2%e+1");
+
+    }
+}
